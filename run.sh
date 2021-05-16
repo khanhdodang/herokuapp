cd3e3c5e7cb1443fc3c0f4522f1c3d3a09ed3121
Khanh Do <khanhdo.pmp@gmail.com>
2021-05-16 14:34:16 +0700

    Update Files

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 virtualenv env
 source ./env/bin/activate
 pip install -r requirements.txt
-py.test --alluredir=%allure_result_folder% TestSuites/testsuite.py
-allure serve %allure_result_folder%
+py.test --alluredir=Reports TestSuites/testsuite.py
+allure serve Reports
 source ./env/bin/deactivate
